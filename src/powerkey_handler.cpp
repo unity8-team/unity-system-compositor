160f4950dbe0e3498f823a73bfe9fd0519205b6f
Andreas Pokorny <andreas.pokorny@canonical.com>
2014-11-06 11:16:21 +0100

    Remove debug output

--- src/powerkey_handler.cpp
+++ src/powerkey_handler.cpp
@@ -19,21 +19,10 @@
 
 #include <mir/time/timer.h>
 
-#include <iostream>
 #include <chrono>
 #include <algorithm>
 
 namespace mi = mir::input;
-
-namespace
-{
-inline std::ostream & log()
-{
-    auto now = std::chrono::high_resolution_clock::now();
-    auto val = std::chrono::duration_cast<std::chrono::milliseconds>(now.time_since_epoch()).count();
-    return std::cout << val << ":";
-}
-}
 
 usc::PowerKeyHandler::PowerKeyHandler(mir::time::Timer& timer,
                                  std::chrono::milliseconds power_key_ignore_timeout,
@@ -70,10 +59,8 @@ bool usc::PowerKeyHandler::handle(MirEvent const& event)
 
 void usc::PowerKeyHandler::power_key_down()
 {
-    log() << __FUNCTION__ << std::endl;
     if (try_transistion(KeyState::Released, KeyState::Pressed))
     {
-        log() << __FUNCTION__ << " rescheduling timers in " << power_key_ignore_timeout.count() << ", "<< shutdown_timeout.count() << std::endl;
         long_press_alarm->reschedule_in(power_key_ignore_timeout);
         shutdown_alarm->reschedule_in(shutdown_timeout);
         std::for_each(begin(key_state_listener), end(key_state_listener), [](usc::PowerKeyStateListener* item){item->power_key_down();});
@@ -82,22 +69,18 @@ void usc::PowerKeyHandler::power_key_down()
 
 void usc::PowerKeyHandler::power_key_up()
 {
-    log() << __FUNCTION__ << std::endl;
     if (try_transistion(KeyState::Pressed, KeyState::Released))
     {
-        log() << __FUNCTION__ << " cancel timeouts" << std::endl;
         shutdown_alarm->cancel();
         long_press_alarm->cancel();
         std::for_each(begin(key_state_listener), end(key_state_listener), [](usc::PowerKeyStateListener* item){item->power_key_short();});
     }
     else if (try_transistion(KeyState::LongPressed, KeyState::Released))
     {
-        log() << __FUNCTION__ << " cancel shutdown" << std::endl;
         shutdown_alarm->cancel();
     }
     else if (try_transistion(KeyState::VeryLongPressed, KeyState::Released))
     {
-        log() << __FUNCTION__ << " after very long" << std::endl;
         // both alarms already fired
     }
     std::for_each(begin(key_state_listener), end(key_state_listener), [](usc::PowerKeyStateListener* item){item->power_key_up();});
@@ -105,20 +88,16 @@ void usc::PowerKeyHandler::power_key_up()
 
 void usc::PowerKeyHandler::shutdown_alarm_notification()
 {
-    log() << __FUNCTION__ << std::endl;
     if (try_transistion(KeyState::LongPressed, KeyState::VeryLongPressed))
     {
-        log() << __FUNCTION__ << " just inform listener" << std::endl;
         std::for_each(begin(key_state_listener), end(key_state_listener), [](usc::PowerKeyStateListener* item){item->power_key_very_long();});
     }
 }
 
 void usc::PowerKeyHandler::long_press_notification()
 {
-    log() << __FUNCTION__ << std::endl;
     if (try_transistion(KeyState::Pressed, KeyState::LongPressed))
     {
-        log() << __FUNCTION__ << " just inform listener" << std::endl;
         std::for_each(begin(key_state_listener), end(key_state_listener), [](usc::PowerKeyStateListener* item){item->power_key_long();});
     }
 }
